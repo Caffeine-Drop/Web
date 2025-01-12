7cfc460ef6dc21cf54ff1b8cba14605d8ae46c2b
Minchaez <rlawlsdl0730@gmail.com>
2025-01-12 19:53:34 +0900

    Feat : 스크롤 가능 기능 추가
    Web상에서 간헐적으로 스크롤이 되지 않는 현상 있어. 해당 코드 추가하였음.

--- CaffeineDrop/App.js
+++ CaffeineDrop/App.js
@@ -1,7 +1,8 @@
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 
 // 각 페이지 임포트
 import DetailPage from "./pages/detailpage";
@@ -9,6 +10,11 @@ import HomePage from "./pages/homepage";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      document.body.style.overflow = "auto";
+    }
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>

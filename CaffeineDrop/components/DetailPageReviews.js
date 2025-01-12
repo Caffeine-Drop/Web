4b719d9a985ff8100c22f721a49d4a81dd53b18c
Minchaez <rlawlsdl0730@gmail.com>
2025-01-12 19:59:33 +0900

    Feat : 디테일 페이지 이용자 리뷰 조회 정적 디자인 구현

---
+++ CaffeineDrop/components/DetailPageReviews.js
@@ -0,0 +1,305 @@
+import React from "react";
+import styled from "styled-components/native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+
+// 이미지 임포트
+import ProfileImage from "../assets/DetailPage/Profile.png";
+import BlankMenuImage from "../assets/DetailPage/blankMenuImg.png";
+import SignatureMenuImg1 from "../assets/DetailPage/signatureMenuImg1.png";
+import SignatureMenuImg2 from "../assets/DetailPage/signatureMenuImg2.png";
+import SignatureMenuImg3 from "../assets/DetailPage/signatureMenuImg3.png";
+
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from "../utils/responsive";
+
+export default function DetailPageReviews() {
+  return (
+    <Container>
+      <Header>
+        <ReviewTitle>
+          <Title>이용자 리뷰</Title>
+          <ReviewCount>15건</ReviewCount>
+        </ReviewTitle>
+        <ReviewViewMoreButton>
+          <ReviewViewMoreButtonText>더보기 &gt;</ReviewViewMoreButtonText>
+        </ReviewViewMoreButton>
+      </Header>
+      <ReviewList>
+        <Review>
+          <ReviewUserInfo>
+            <ReviewUserProfileImage source={ProfileImage} />
+            <ReviewUser>
+              <ReviewUserNickName>닉네임</ReviewUserNickName>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <ReviewCreatedAt>2025.01.12</ReviewCreatedAt>
+                <ReviewRating>4.0</ReviewRating>
+              </View>
+            </ReviewUser>
+          </ReviewUserInfo>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: "center",
+            }}
+          >
+            <ReviewPictures>
+              <ReviewPictureImage
+                source={BlankMenuImage}
+                resizeMode="stretch"
+              />
+              <ReviewPictureImage
+                source={SignatureMenuImg1}
+                resizeMode="stretch"
+              />
+              <ReviewPictureImage
+                source={SignatureMenuImg2}
+                resizeMode="stretch"
+              />
+              <ReviewPictureImage
+                source={SignatureMenuImg3}
+                resizeMode="stretch"
+              />
+            </ReviewPictures>
+          </ScrollView>
+          <ReviewContent>
+            <Text
+              style={{
+                color: "#000",
+                fontSize: responsiveFontSize(12),
+                fontWeight: "400",
+                lineHeight: responsiveFontSize(16),
+                letterSpacing: "-0.3",
+                marginVertical: 0,
+                paddingVertical: 0,
+              }}
+            >
+              도시 위에 소음 넘쳐나는 트러블 여유 없는 걸음 이건 마치 정글
+              멍하니 또 한숨이
+            </Text>
+          </ReviewContent>
+        </Review>
+        {/* 리뷰 추가 */}
+        <Review>
+          <ReviewUserInfo>
+            <ReviewUserProfileImage source={ProfileImage} />
+            <ReviewUser>
+              <ReviewUserNickName>닉네임</ReviewUserNickName>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <ReviewCreatedAt>2025.01.12</ReviewCreatedAt>
+                <ReviewRating>4.0</ReviewRating>
+              </View>
+            </ReviewUser>
+          </ReviewUserInfo>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: "center",
+            }}
+          >
+            <ReviewPictures>
+              <ReviewPictureImage
+                source={BlankMenuImage}
+                resizeMode="stretch"
+              />
+              <ReviewPictureImage
+                source={SignatureMenuImg1}
+                resizeMode="stretch"
+              />
+              <ReviewPictureImage
+                source={SignatureMenuImg2}
+                resizeMode="stretch"
+              />
+              <ReviewPictureImage
+                source={SignatureMenuImg3}
+                resizeMode="stretch"
+              />
+            </ReviewPictures>
+          </ScrollView>
+          <ReviewContent>
+            <Text
+              style={{
+                color: "#000",
+                fontSize: responsiveFontSize(12),
+                fontWeight: "400",
+                lineHeight: responsiveFontSize(16),
+                letterSpacing: "-0.3",
+                marginVertical: 0,
+                paddingVertical: 0,
+              }}
+            >
+              도시 위에 소음 넘쳐나는 트러블 여유 없는 걸음 이건 마치 정글
+              멍하니 또 한숨이
+            </Text>
+          </ReviewContent>
+        </Review>
+      </ReviewList>
+      {/* 하단 리뷰 추가 버튼 */}
+      <ReviewAddButton>
+        <ReviewAddButtonText>+</ReviewAddButtonText>
+      </ReviewAddButton>
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  width: 100%;
+  padding: ${responsiveHeight(44.5)}px 24px 0 24px;
+`;
+
+const Header = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ReviewTitle = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${responsiveWidth(8)}px;
+`;
+
+const Title = styled.Text`
+  color: #000;
+  font-size: ${responsiveFontSize(16)}px;
+  font-weight: 600;
+  line-height: ${responsiveFontSize(22.08)}px;
+  letter-spacing: -0.4px;
+  text-transform: uppercase;
+`;
+
+const ReviewCount = styled.Text`
+  color: #666;
+  text-align: center;
+  font-size: ${responsiveFontSize(14)}px;
+  font-weight: 500;
+  line-height: ${responsiveFontSize(19.32)}px;
+  letter-spacing: -0.35px;
+  text-transform: uppercase;
+`;
+
+const ReviewViewMoreButton = styled.TouchableOpacity`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${responsiveWidth(8)}px;
+`;
+
+const ReviewViewMoreButtonText = styled.Text`
+  color: #666;
+  font-size: ${responsiveFontSize(14)}px;
+  font-weight: 500;
+  line-height: ${responsiveFontSize(19.32)}px;
+  letter-spacing: -0.35px;
+`;
+
+const ReviewList = styled.View`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Review = styled.View`
+  display: flex;
+  flex-direction: column;
+  padding: ${responsiveHeight(16)}px 0;
+`;
+
+const ReviewUserInfo = styled.View`
+  display: flex;
+  flex-direction: row;
+  gap: ${responsiveWidth(12)}px;
+`;
+
+const ReviewUserProfileImage = styled.Image`
+  width: ${responsiveWidth(34)}px;
+  height: ${responsiveWidth(34)}px;
+`;
+
+const ReviewUser = styled.View`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ReviewUserNickName = styled.Text`
+  color: #000;
+  font-size: ${responsiveFontSize(16)}px;
+  font-weight: 600;
+  line-height: ${responsiveFontSize(22.08)}px;
+  letter-spacing: -0.4px;
+  text-transform: uppercase;
+`;
+
+const ReviewCreatedAt = styled.Text`
+  color: #666;
+  font-size: ${responsiveFontSize(14)}px;
+  font-weight: 500;
+  line-height: ${responsiveFontSize(19.32)}px;
+  letter-spacing: -0.35px;
+`;
+
+const ReviewRating = styled.Text`
+  color: #666;
+  font-size: ${responsiveFontSize(14)}px;
+  font-weight: 500;
+  line-height: ${responsiveFontSize(19.32)}px;
+  letter-spacing: -0.35px;
+`;
+
+const ReviewPictures = styled.View`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  padding: 12px 0;
+  gap: ${responsiveWidth(4)}px;
+`;
+
+const ReviewPictureImage = styled.Image`
+  height: ${responsiveWidth(80)}px;
+  max-width: ${responsiveWidth(120)}px;
+  border-radius: ${responsiveWidth(4)}px;
+`;
+
+const ReviewContent = styled.View`
+  display: flex;
+  width: ${responsiveWidth(312)}px;
+  flex-wrap: wrap;
+`;
+
+const ReviewAddButton = styled.TouchableOpacity`
+  display: flex;
+  width: ${responsiveWidth(312)}px;
+  padding: ${responsiveHeight(8)}px 0;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #d9d9d9;
+  border-radius: ${responsiveWidth(1)}px;
+  margin-bottom: ${responsiveHeight(107)}px;
+`;
+
+const ReviewAddButtonText = styled.Text`
+  color: #000;
+  font-size: ${responsiveFontSize(24)}px;
+  font-weight: 600;
+  line-height: ${responsiveFontSize(32)}px;
+  letter-spacing: -0.4px;
+`;

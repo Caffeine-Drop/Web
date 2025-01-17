cf95aa4d6e99a6d34717c33e6e495e9603c12c05
Minchaez <rlawlsdl0730@gmail.com>
2025-01-18 00:56:06 +0900

    Feat : detailPage 스크롤 위치에 따른 Nav바 구현

--- CaffeineDrop/pages/detailpage.js
+++ CaffeineDrop/pages/detailpage.js
@@ -1,189 +1,137 @@
-import React from "react";
-import { View, Text, Image, Button, ScrollView } from "react-native";
+import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Animated,
+} from "react-native";
+import styled from "styled-components/native";
+
 import {
   responsiveFontSize,
   responsiveWidth,
   responsiveHeight,
 } from "../utils/responsive";
-import styled from "styled-components/native";
-
-// 이미지 파일 경로
-import detailImage from "../assets/DetailPage/detailMainImg.png";
-import specialtyCoffeeLogo from "../assets/DetailPage/specialtyCoffeeLogo.png";
-import distanceLogo from "../assets/DetailPage/distanceLogo.png";
-import caffeeLikeDefalut from "../assets/DetailPage/caffeeLikeDefalut.png";
-import stars from "../assets/DetailPage/stars.png";
 
 // 컴포넌트
-import BackButton from "../components/BackButton";
-import DetailPageImg from "../components/DetailPageImg";
-import DetailpageMenu from "../components/DetailPageMenu";
-import DetailPageMap from "../components/DetailPageMap";
-import DetailPageCategory from "../components/DetailPageCategory";
-import DetailPageReviews from "../components/DetailPageReviews";
+import DetailPageHeader from "../components/DetailPageHeader";
 import DetailPageWriteReviewButton from "../components/DetailPageWriteReviewButton";
-import DetailPageReviewOverView from "../components/DetailPageReviewOverView";
+import DetailPageHome from "../pages/detailpagehome";
+import DetailPageReview from "../pages/detailpagereview";
+import DetailPageImage from "../pages/detailpageimage";
+import DetailPageBeansInfo from "../pages/detailpagebeansinfo";
+import BackButton from "../components/BackButton";
 
 export default function DetailPage({ navigation }) {
-  return (
-    <View style={{ flex: 1 }}>
-      <ScrollView>
-        <Container>
-          <DetailImage
-          style={{
-            height: responsiveHeight(400),
-          }}
-          source={detailImage}
-        />
-        <Header>
-          <BackButton onPress={() => navigation.goBack()} />
-          <Text
-            style={{
-              fontSize: responsiveFontSize(18),
-              fontWeight: "bold",
-              color: "white",
-              justifyContent: "center",
-            }}
-          >
-            카페 상세보기
-          </Text>
-        </Header>
-        {/* 이 부분 추후 사용 또는 파일 확장자를 .svg로 변경하여 화질 깨지는 문제 해결 예정*/}
-        {/* <View
-        style={{
-          position: "absolute",
-          bottom: 0,
-          backgroundColor: "#FAFAFA",
-          width: scaleWidth(100), // 88이 원래 사이즌데, 88로 하니까 공백 생김. 일단 100으로 해놓은 상태.
-          height: scaleHeight(22),
-          borderRadius: 4,
-          left: scaleWidth(24),
-          top: scaleHeight(222),
-        }}
-      /> */}
-        {/* <Text
-        style={{
-          position: "absolute",
-          fontFamily: "Pretendard",
-          fontSize: scaleWidth(10),
-          fontWeight: "medium",
-          justifyContent: "center",
-          left: scaleWidth(32),
-          top: scaleHeight(226),
-        }}
-      >
-        Specialty Coffee
-      </Text> */}
-        <Image
-          source={specialtyCoffeeLogo}
-          style={{
-            position: "absolute",
-            width: responsiveWidth(88),
-            height: responsiveHeight(20),
-            left: responsiveWidth(24),
-            top: responsiveHeight(226),
-          }}
-        />
-        <TitleText>언힙커피로스터스</TitleText>
-        <Text
-          style={{
-            position: "absolute",
-            fontFamily: "Pretendard",
-            fontSize: responsiveFontSize(14),
-            fontWeight: "regular",
-            lineHeight: responsiveHeight(19.32),
-            color: "#fafafa",
-            justifyContent: "center",
-            left: responsiveWidth(24),
-            top: responsiveHeight(299),
-          }}
-        >
-          인천 미추홀구 인하로67번길 6 2층
-        </Text>
-        <Image
-          source={distanceLogo}
-          style={{
-            position: "absolute",
-            left: responsiveWidth(24),
-            top: responsiveHeight(326),
-          }}
-        />
-        {/* 이 부분 추후 사용 또는 파일 확장자를 .svg로 변경하여 화질 깨지는 문제 해결 예정*/}
-        {/* <View style={{
-        position: "absolute",
-        width: scaleWidth(26),
-        height: scaleHeight(18),
-        backgroundColor: "#fafafa",
-        borderRadius: 8,
-        left: scaleWidth(24),
-        top: scaleHeight(326),
-        }} /> */}
-        <Text
-          style={{
-            position: "absolute",
-            fontFamily: "Pretendard",
-            fontSize: responsiveFontSize(12),
-            fontWeight: "regular",
-            lineHeight: responsiveHeight(16.56),
-            color: "#fafafa",
-            left: responsiveWidth(54),
-            top: responsiveHeight(326),
-          }}
-        >
-          1.2km
-        </Text>
-        <View
-          style={{
-            position: "absolute",
-            flexDirection: "row",
-            alignItems: "center",
-            gap: responsiveWidth(4),
-            left: responsiveWidth(24),
-            top: responsiveHeight(356.5),
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: "Pretendard",
-              fontSize: responsiveFontSize(14),
-              fontWeight: "normal",
-              lineHeight: responsiveHeight(19.32),
-              color: "#fafafa",
-            }}
-          >
-            4.0
-          </Text>
-          <Image
-            source={stars}
-            style={{
-              width: responsiveWidth(88),
-              height: responsiveHeight(20),
+  const [selectedTab, setSelectedTab] = useState("home");
+  const fadeAnim = useState(new Animated.Value(1))[0];
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isNavBarFixed, setIsNavBarFixed] = useState(false);
+  const scrollViewRef = useRef(null);
+
+  const handleScroll = (event) => {
+    const scrollY = event.nativeEvent.contentOffset.y;
+    setIsScrolled(scrollY > 10);
+    setIsNavBarFixed(scrollY > 327);
+  };
+
+  const handleTabPress = (tab) => {
+    setSelectedTab(tab);
+    fadeAnim.setValue(0);
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const renderPage = () => {
+    switch (selectedTab) {
+      case "home":
+        return (
+          <DetailPageHome
+            selectedTab={selectedTab}
+            onViewMoreImgPress={() => handleTabPress("image")}
+            onViewMoreReviewPress={() => {
+              handleTabPress("review");
+              if (scrollViewRef.current) {
+                scrollViewRef.current.scrollTo({ y: 0, animated: true });
+              }
             }}
           />
+        );
+      case "review":
+        return <DetailPageReview selectedTab={selectedTab} />;
+      case "image":
+        return (
+          <DetailPageImage selectedTab={selectedTab} navigation={navigation} />
+        );
+      case "beansinfo":
+        return <DetailPageBeansInfo />;
+    }
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isScrolled && (
+        <FixedHeader>
+          <View style={{ width: "100%", flexDirection: "column" }}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <BackButton style={{ position: "absolute" }} onPress={() => navigation.goBack()} />
+              <FixedHeaderText>언힙커피로스터스</FixedHeaderText>
+            </View>
+            {isNavBarFixed && (
+              <NavBar>
+                {["home", "review", "image", "beansinfo"].map((tab) => (
+                  <TouchableOpacity
+                    key={tab}
+                    onPress={() => handleTabPress(tab)}
+                  >
+                    <NavTab isSelected={selectedTab === tab}>
+                      <TabText isSelected={selectedTab === tab}>
+                        {tab === "home"
+                          ? "홈"
+                          : tab === "review"
+                          ? "리뷰"
+                          : tab === "image"
+                          ? "이미지"
+                          : "원두 정보"}
+                      </TabText>
+                    </NavTab>
+                  </TouchableOpacity>
+                ))}
+              </NavBar>
+            )}
+          </View>
+        </FixedHeader>
+      )}
+      <ScrollView onScroll={handleScroll} scrollEventThrottle={16}>
+        <DetailPageHeader navigation={navigation} isScrolled={isScrolled} />
+        <View>
+          <Container>
+            <NavBar>
+              {["home", "review", "image", "beansinfo"].map((tab) => (
+                <TouchableOpacity key={tab} onPress={() => handleTabPress(tab)}>
+                  <NavTab isSelected={selectedTab === tab}>
+                    <TabText isSelected={selectedTab === tab}>
+                      {tab === "home"
+                        ? "홈"
+                        : tab === "review"
+                        ? "리뷰"
+                        : tab === "image"
+                        ? "이미지"
+                        : "원두 정보"}
+                    </TabText>
+                  </NavTab>
+                </TouchableOpacity>
+              ))}
+            </NavBar>
+            <Animated.View style={{ opacity: fadeAnim }}>
+              {renderPage()}
+            </Animated.View>
+          </Container>
         </View>
-        <Image
-          source={caffeeLikeDefalut}
-          style={{
-            position: "absolute",
-            width: 40,
-            height: 40,
-            right: responsiveWidth(24),
-            top: responsiveHeight(336),
-          }}
-        />
-        <NavBar>
-          <NavTabSelected>홈</NavTabSelected>
-          <NavTabNonSelected>리뷰</NavTabNonSelected>
-          <NavTabNonSelected>이미지</NavTabNonSelected>
-          <NavTabNonSelected>원두 정보</NavTabNonSelected>
-        </NavBar>
-      </Container>
-      <DetailPageImg />
-      <DetailpageMenu />
-      <DetailPageMap />
-      <DetailPageCategory />
-      <DetailPageReviews />
-      <DetailPageReviewOverView />
       </ScrollView>
       <DetailPageWriteReviewButton />
     </View>
@@ -195,35 +143,6 @@ const Container = styled.View`
   position: relative;
 `;
 
-const DetailImage = styled.Image`
-  width: 100%;
-  top: 0;
-  left: 0;
-`;
-
-const Header = styled.View`
-  position: absolute;
-  top: 50px;
-  width: 100%;
-  flex-direction: row;
-  align-items: center;
-  padding: 0;
-  justify-content: center;
-`;
-
-const TitleText = styled.Text`
-  position: absolute;
-  font-family: "Pretendard";
-  font-size: 28px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: ${responsiveHeight(38.64)};
-  letter-spacing: -0.7px;
-  color: #fafafa;
-  left: ${responsiveWidth(24)};
-  top: ${responsiveHeight(248)};
-`;
-
 const NavBar = styled.View`
   display: flex;
   flex-direction: row;
@@ -232,26 +151,46 @@ const NavBar = styled.View`
   gap: 12px;
   bottom: 0;
   width: 100%;
-  height: 46px;
+  height: 48px;
   background-color: #756555;
 `;
 
-const NavTabSelected = styled.Text`
-  font-size: 16px;
-  font-family: "Pretendard";
-  padding: 12px 12px;
-  font-style: semibold;
-  line-height: 22.08px;
-  font-weight: 600;
-  color: #ffffff;
+const NavTab = styled.View`
+  padding: 12px 16px;
+  border-bottom-width: 2px;
+  border-bottom-color: ${(props) =>
+    props.isSelected ? "#fafafa" : "transparent"};
 `;
 
-const NavTabNonSelected = styled.Text`
-  font-size: ${responsiveFontSize(16)};
-  font-family: "Pretendard";
-  padding: 12px 12px;
-  font-style: normal;
-  line-height: 22.08px;
-  font-weight: 400;
-  color: #999999;
+const TabText = styled.Text`
+  color: ${(props) => (props.isSelected ? "#ffffff" : "#999999")};
+  font-size: ${responsiveFontSize(16)}px;
+  font-weight: 600;
+  line-height: ${responsiveHeight(22.08)}px;
+  letter-spacing: -0.04px;
+`;
+
+const FixedHeader = styled.View`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 38px;
+  background-color: #756555;
+  z-index: 1000;
+`;
+
+const FixedHeaderText = styled.Text`
+  overflow: hidden;
+  width: 100%;
+  height: ${responsiveHeight(56)}px;
+  padding: 15px 0;
+  color: #fafafa;
+  text-align: center;
+  text-overflow: ellipsis;
+  font-size: ${responsiveFontSize(18)}px;
+  font-weight: 600;
+  line-height: ${responsiveHeight(24.84)}px;
+  letter-spacing: -0.45px;
 `;

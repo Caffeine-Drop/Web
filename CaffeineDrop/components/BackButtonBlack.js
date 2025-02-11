23c71ad16c35f8349a998e647631d7ff0808d643
Minchaez <rlawlsdl0730@gmail.com>
2025-02-12 07:41:23 +0900

    Fix : 검정색 뒤로가기 버튼 반응형으로 변경

--- CaffeineDrop/components/BackButtonBlack.js
+++ CaffeineDrop/components/BackButtonBlack.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { TouchableOpacity, Image } from "react-native";
 
+import { responsiveWidth } from "../utils/responsive";
 import BackButtonIconBlack from "../assets/TermsPage/BackButtonBlack.svg";
 
 const BackButtonBlack = ({ onPress, style }) => {
   return (
-    <TouchableOpacity style={[{ marginLeft: 24, zIndex: 1001 }, style]} onPress={onPress}>
+    <TouchableOpacity style={[{ marginLeft: 24, zIndex: 1001, width: responsiveWidth(24), height: responsiveWidth(24) }, style]} onPress={onPress}>
       <BackButtonIconBlack />
     </TouchableOpacity>
   );

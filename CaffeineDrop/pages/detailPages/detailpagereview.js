43be5a090c17fe786bc5d5683b38e74738d890cd
Minchaez <rlawlsdl0730@gmail.com>
2025-02-19 21:04:42 +0900

    Feat : 디테일 페이지 리뷰 탭 API연동

--- CaffeineDrop/pages/detailPages/detailpagereview.js
+++ CaffeineDrop/pages/detailPages/detailpagereview.js
@@ -8,22 +8,45 @@ import DetailPageEmpty from "../../components/detailPage/DetailPageEmpty";
 export default function DetailPageReview({
   selectedTab,
   onViewMoreReviewPress,
+  reviews,
+  ratings,
 }) {
+  console.log("reviews: ", reviews.data.reviews);
+  console.log("tastingRating: ", ratings.data.detailedRatings[0].rating);
+  console.log("interiorRating: ", ratings.data.detailedRatings[1].rating);
+  console.log("cleanlinessRating: ", ratings.data.detailedRatings[2].rating);
+  console.log(
+    "costPerformanceRating: ",
+    ratings.data.detailedRatings[3].rating
+  );
   return (
     <View style={{ flex: 1 }}>
       <ScrollView>
-        <DetailPageReviewOverView />
-        <DetailPageReviews
-          selectedTab={selectedTab}
-          onViewMoreReviewPress={onViewMoreReviewPress}
-        />
-        <DetailPageEmpty
-          mainText1="아직 등록된"
-          mainText2="리뷰가 없어요"
-          subText1="카페 방문 후 리뷰를"
-          subText2="남겨 보세요"
-          selectedTab={selectedTab}
-        />
+        {reviews.data.reviews.length > 0 ? (
+          <>
+            <DetailPageReviewOverView
+              reviews={reviews}
+              averageRating={ratings.data.averageRating}
+              tastingRating={ratings.data.detailedRatings[0].rating}
+              interiorRating={ratings.data.detailedRatings[1].rating}
+              cleanlinessRating={ratings.data.detailedRatings[2].rating}
+              costPerformanceRating={ratings.data.detailedRatings[3].rating}
+            />
+            <DetailPageReviews
+              selectedTab={selectedTab}
+              onViewMoreReviewPress={onViewMoreReviewPress}
+              reviews={reviews}
+            />
+          </>
+        ) : (
+          <DetailPageEmpty
+            mainText1="아직 등록된"
+            mainText2="리뷰가 없어요"
+            subText1="카페 방문 후 리뷰를"
+            subText2="남겨 보세요"
+            selectedTab={selectedTab}
+          />
+        )}
       </ScrollView>
     </View>
   );
